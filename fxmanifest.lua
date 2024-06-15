96bbd2ba36d81a763d18473aa3df4be90a0076a9
skun.pro <91133634+skunpro@users.noreply.github.com>
2024-06-15 16:26:57 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,16 +5,13 @@ lua54 'yes'
 author 'notaskun - https://skun.pro'
 description 'Command /wezwij [ID] / default ui'
 
-version '1.0.0'
-
-require 'mysql-async'
+version '1.0.1'
 
 client_scripts {
     'client/skun-client.lua'
 }
 
 server_scripts {
-    '@mysql-async/lib/MySQL.lua',
     'server/skun-server.lua'
 }
 
@@ -25,7 +22,3 @@ files {
 }
 
 shared_script '@es_extended/imports.lua'
-
-dependencies {
-	'mysql-async'
-}
